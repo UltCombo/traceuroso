ccabf5c8257c229d4e5459dbbb1bd2a0c0135eae
UltCombo <ultcombo@gmail.com>
2014-07-29 03:05:57 -0300

    Only load a single Traceur singleton, see https://github.com/google/traceur-compiler/issues/1209#issue-38851768

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var path = require('path'),
-	traceur = require('traceur');
+global._traceuroso = global._traceuroso || {};
+
+// Only load a single Traceur singleton, see https://github.com/google/traceur-compiler/issues/1209#issue-38851768
+var requireMakeDefault = _traceuroso.requireMakeDefault = _traceuroso.requireMakeDefault || require('traceur').require.makeDefault,
+	path = require('path');
 
 module.exports = function(packageRoot, entryPoint) {
 	packageRoot = path.resolve(packageRoot);
 	entryPoint = entryPoint || 'index';
 
-	traceur.require.makeDefault(function(filepath) {
+	requireMakeDefault(function(filepath) {
 		return filepath.startsWith(packageRoot + path.sep) && !filepath.startsWith(path.join(packageRoot, 'node_modules') + path.sep);
 	}, { experimental: true });
 
